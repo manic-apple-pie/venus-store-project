a65523cada4f9bc9e36a72a13cf340148883b27b
manic-apple-pie <106272954+manic-apple-pie@users.noreply.github.com>
2022-05-26 11:19:53 +0300

    fix: minor fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,28 @@ button.onclick = function click() {
     }
 }
 
+const newArrival = document.getElementById('newArrivals-el')
+let dataReload = document.querySelectorAll(["data-reload"])
+
+let language = {
+    en: {
+        newArrivals: "New arrivals"
+    },
+    ro: {
+        newArrivals: "Cele mai noi"
+    }
+}
+
+if (window.location.hash) {
+    if (window.location.hash === "#ro") {
+        newArrival.textContent = language.ro.newArrivals
+    } else {
+        newArrival.textContent = language.en.newArrivals
+    }
+}
+
+for (i = 0; i <= dataReload.length; i++) {
+    dataReload[i].onclick = function() {
+        location.reload(true)
+    }
+}
